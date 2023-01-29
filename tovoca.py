339f620447e9a240041d0d945356f52d5b814701
dong <459188369@qq.com>
2023-01-29 15:02:56 +0800

    adjust folder structure of VOCA dataset

--- tovoca.py
+++ tovoca.py
@@ -28,61 +28,42 @@ pkl = {}
 os.makedirs('owndata/vertices_npy',  exist_ok=True)
 os.makedirs('owndata/wav',  exist_ok=True)
 
-root_path = '/mnt/audio_exp_dataset'
-speakers = os.listdir(root_path)
-for speaker in speakers:
-    sentences = os.listdir(os.path.join(root_path, speaker))
-    total_verts = []
-    total_count = 0
-    torch.cuda.empty_cache()
-    for sentence in sentences:
-        wav_path = os.path.join(root_path, speaker, sentence, 'audio.wav')
-        exp_jaw = os.path.join(root_path, speaker, sentence, 'exp_jaw.npy')
+def type2save(root_path):
+    speakers = os.listdir(root_path)
+    for speaker in speakers:
+        sentences = os.listdir(os.path.join(root_path, speaker))
+        total_verts = []
+        total_count = 0
+        torch.cuda.empty_cache()
+        for sentence in sentences:
+            wav_path = os.path.join(root_path, speaker, sentence, 'audio.wav')
+            exp_jaw = os.path.join(root_path, speaker, sentence, 'exp_jaw.npy')
 
-        prediction = torch.tensor(np.load(exp_jaw)).to(device="cuda")
+            prediction = torch.tensor(np.load(exp_jaw)).to(device="cuda")
 
-        pose_params = torch.zeros([prediction.shape[0], 15]).to(device="cuda")
-        pose_params[:, 6:9] = prediction[:, 50:]
-        expression_params = torch.zeros([prediction.shape[0], 100]).to(device="cuda")
-        expression_params[:, :50] = prediction[:, :50]
-        shape_params = torch.zeros([1, 300]).to(device="cuda")
+            pose_params = torch.zeros([prediction.shape[0], 15]).to(device="cuda")
+            pose_params[:, 6:9] = prediction[:, -3:]
+            expression_params = torch.zeros([prediction.shape[0], 100]).to(device="cuda")
+            if(prediction.shape[1]==53):
+                expression_params[:, :50] = prediction[:, :-3]
+            else:
+                expression_params[:, :] = prediction[:, :-3]
+            shape_params = torch.zeros([1, 300]).to(device="cuda")
 
-        verts, landmarks3d = flame(pose_params.shape[0], \
-                                    shape_params=shape_params, \
-                                    expression_params=expression_params, \
-                                    pose_params=pose_params)
+            verts, landmarks3d = flame(pose_params.shape[0], \
+                                        shape_params=shape_params, \
+                                        expression_params=expression_params, \
+                                        pose_params=pose_params)
 
-        verts = np.array(verts.cpu().numpy().reshape(verts.shape[0], -1))
-        print(verts.shape)
-        total_count += verts.shape[0]
-        total_verts.append(verts.sum(0))
-        np.save('owndata/vertices_npy/{}_{}.npy'.format(speaker, sentence), verts)
-        shutil.copy(wav_path, 'owndata/wav/{}_{}.wav'.format(speaker, sentence))
-    
-    pkl[speaker] = np.array(total_verts).sum(0)/total_count
+            verts = np.array(verts.cpu().numpy().reshape(verts.shape[0], -1))
+            print(verts.shape)
+            total_count += verts.shape[0]
+            total_verts.append(verts.sum(0))
+            np.save('owndata/vertices_npy/{}_{}.npy'.format(speaker, sentence), verts)
+            shutil.copy(wav_path, 'owndata/wav/{}_{}.wav'.format(speaker, sentence))
+        
+        pkl[speaker] = np.array(total_verts).sum(0)/total_count
 
-
-root_path = '/mnt/vocaset'
-speakers = [name for name in os.listdir(root_path) if os.path.isdir(os.path.join(root_path, name))]
-for speaker in speakers:
-    sentences = os.listdir(os.path.join(root_path, speaker, 'mesh_processed'))
-    total_verts = []
-    total_count = 0
-    torch.cuda.empty_cache()
-    for sentence in sentences:
-        wav_path = os.path.join(root_path, speaker, 'audio_processed', sentence+'.wav')
-        objs = os.listdir(os.path.join(root_path, speaker, 'mesh_processed', sentence))
-        objs.sort()
-        verts = []
-        for obj in objs:
-            verts.append(trimesh.load(os.path.join(root_path, speaker, 'mesh_processed', sentence, obj), process=False, maintain_order=True).vertices.reshape(-1))
-        verts = np.array(verts)
-        print(verts.shape)
-        total_count += verts.shape[0]
-        total_verts.append(verts.sum(0))
-        np.save('owndata/vertices_npy/{}_{}.npy'.format(speaker, sentence), verts)
-        shutil.copy(wav_path, 'owndata/wav/{}_{}.wav'.format(speaker, sentence))
-
-    pkl[speaker] = np.array(total_verts).sum(0)/total_count
+type2save('/mnt/ludong/audio_exp_dataset_filter')
 
 pickle.dump(pkl, open('owndata/templates.pkl', 'wb'))
